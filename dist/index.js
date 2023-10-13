5d7829b0671a768bfd4cda2c3e411f98a291200b
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2023-10-13 15:39:40 +0800

    Add Electron, remove node_modules and dist from tracking

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Enigma_1 = require("./Enigma");
-const enigma = new Enigma_1.Enigma('aes-256-ctr', 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3');
-const encrypted_password = enigma.encrypt("password");
-console.log(encrypted_password);
-console.log(enigma.decrypt(encrypted_password));
